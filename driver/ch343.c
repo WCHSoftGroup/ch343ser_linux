52a91c8f8e5327466aca9ac592bb2c94f503d972
Scott Ellis <scotte@warped.com>
2024-11-18 12:59:55 -0800

    unaligned.sh moved from asm/ to linux/ in kernel 6.12.0

--- driver/ch343.c
+++ driver/ch343.c
@@ -52,7 +52,12 @@
 #include <linux/usb/cdc.h>
 #include <linux/version.h>
 #include <asm/byteorder.h>
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0))
+#include <linux/unaligned.h>
+#else
 #include <asm/unaligned.h>
+#endif
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0))
 #include <linux/sched/signal.h>
